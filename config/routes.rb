27a5a251fb39e3854cb080ac7b0855c17f2b040c
vagrant <vagrant@localhost.localdomain>
2017-12-24 05:01:55 +0900

    fifth comm

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
+
+  post '/projects/:project_id/tasks:/:id/toggle' => 'tasks#toggle'
 
   root 'projects#index'
 

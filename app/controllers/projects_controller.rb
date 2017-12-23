8a6b28d0ad9535add9fd452e5c9610fbda2c6828
vagrant <vagrant@localhost.localdomain>
2017-12-24 02:42:35 +0900

    forth comm

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only:[:show, :edit, :update, :destroy]
     # indexアクションを追加
     def index
         # @をつけた変数はviewでそのまま使用可能
@@ -9,7 +10,6 @@ class ProjectsController < ApplicationController
 
       # showアクションを追加
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -28,10 +28,31 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+  
+
   private
 
     def project_params
         params[:project].permit(:title)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
     end
 
 end

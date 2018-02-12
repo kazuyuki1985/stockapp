acf80f6f6c01d0b8bf334ebe768d6e9a52986ea1
vagrant <vagrant@localhost.localdomain>
2018-02-12 10:43:53 +0900

    フォルダとノート数量表記完了&ノート詳細エラー解消

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -70,7 +70,7 @@ class NotesController < StockappController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = current_user.notes.where(params[:id])
+      @note = current_user.notes.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
